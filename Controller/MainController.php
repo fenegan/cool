9e5cc520f6820b7b6b450431984181950dbe7dcc
Killian Vermersch <killian@idequanet.com>
2018-02-08 16:00:59 +0100

    Removed test files an added base structure

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -1,54 +1,11 @@
 <?php
 
 require_once('Cool/BaseController.php');
-require_once('Model/PostManager.php');
-require_once('Model/CommentManager.php');
 
 class MainController extends BaseController
 {
     public function homeAction()
     {
-        $manager = new PostManager();
-        $posts = $manager->getAllPosts();
-        
-        $data = [
-            'posts' => $posts
-        ];
-        
-        return $this->render('home.html.twig', $data);
-    }
-    
-    public function articleAction()
-    {
-        $postManager = new PostManager();
-        $post = $postManager->getPostById(intval($_GET['id']));
-        $commentsManager = new CommentManager();
-        $comments = $commentsManager->getCommentsByPost(intval($_GET['id']));
-        $data = [
-            'article'   => $post,
-            'comments'  => $comments
-        ];
-        
-        return $this->render('article.html.twig', $data);
-        
-    }
-
-    public function contactAction()
-    {
-        return $this->render('contact.html.twig');
-    }
-
-    public function addArticleAction()
-    {
-        if(isset($_POST['title']) && isset($_POST['content'])){
-            $title = $_POST['title'];
-            $content = $_POST['content'];
-            $manager = new PostManager();
-            $posts = $manager->addPost($title, $content);
-            
-            $this->redirectToRoute('home');
-        }
-
-        return $this->render('addArticle.html.twig');
+        return $this->render('home.html.twig');
     }
 }
