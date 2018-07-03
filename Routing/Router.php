09bcf909389386cd39b0056bd1c0ff5b370ea7f5
Neil Richter <neilrichter@Not-A-Macbook-Pro.local>
2018-07-03 23:15:46 +0200

    Added gulpfile for sass preprocessing

--- Routing/Router.php
+++ Routing/Router.php
@@ -12,7 +12,7 @@ class Router
         {
             $data = explode(':', $config['routes'][$request]);
             $controller_name = "Controller\\".$data[0].'Controller';
-            $method_name = $data[1].'Action';
+            $method_name = $data[1];
             
             $controller = new $controller_name;
             $response = call_user_func([$controller, $method_name]);
@@ -23,7 +23,7 @@ class Router
             header("HTTP/1.0 404 Not Found");
             http_response_code(404);
             $controller_name = "Controller\\".'MainController';
-            $method_name = 'error404Action';
+            $method_name = 'error404';
             
             $controller = new $controller_name;
             $response = call_user_func([$controller, $method_name]);
